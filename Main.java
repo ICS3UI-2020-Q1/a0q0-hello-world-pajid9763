a1684e20ad9c8f12eae33c0f301b26f88b92304c
pajid9763 <pajid9763@wrdsb.ca>
2020-09-11 15:55:22 +0000

    Finished Hello World program

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 /**
- *
- * @author 
+ * this program prints hello world to the screen 
+ * @author by Darian
  */
 public class Main {
 
@@ -9,7 +9,8 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // print "Hello World!" to the console
+    System.out.println("Hello World!");
     
   }
 }
